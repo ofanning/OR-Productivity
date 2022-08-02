7a23c5c7650d782f6e5850c28e2cc16aaa7ab675
rockaguirre <107651547+rockaguirre@users.noreply.github.com>
2022-08-01 21:38:06 -0700

    this might fix the blocking issue

--- Code/blocking.js
+++ Code/blocking.js
@@ -1,15 +1,10 @@
-/* https://www.youtube.com/watch?v=dIrXIJ781DQ */
-
-//let cb = document.querySelector('#blocked_button');
-if (window.location.hostname =="www.youtube.come")
-{
-    alert("this is youtube");
-}
-//Check the state of the checked attribute
-/*if (cb.checked)
-{
-    if (window.location.hostname == "www.youtube.com"){
+functionName = function functionName2 (link) {
+    var current = window.location.href;
+    if (current.includes(link)) {
+        document.body.innerHTML = "";
         document.body.innerHTML = "Blocked LOL";
     }
-}*/
+}
+
+functionNAme("youtube.com");
 
