7fbe473e2da4d33516511868a89310da67ec9bb3
rockaguirre <107651547+rockaguirre@users.noreply.github.com>
2022-08-01 19:41:59 -0700

    test

--- Code/timer.js
+++ Code/timer.js
@@ -1,65 +1,21 @@
-var mySeconds;
-var intervalHandle;
+var myVar;
+var timer = document.getElementById("userInput");
+var countDownSeconds;
+function startTime(){ 
+  myVar = setInterval(start, 1000);
+  document.getElementById("timerr").innerHTML = timer.value;
+  countDownSeconds = timer.value;
+} 
 
-function resetPage(){
-	document.getElementById("inputArea").style.display="none";	
-	
-	
-}
-function tick(){
-	var timeDisplay=document.getElementById("time");
-	
-	var min=Math.floor(mySeconds/60);
-	var sec=mySeconds-(min*60);
-	
-	if (sec < 10) {
-		sec="0"+sec;
-	}
-	
-	var message=min.toString()+":"+sec;
-	
-	timeDisplay.innerHTML=message;
-	
-	if(mySeconds===0){
-		alert("Done");
-		clearInterval(intervalHandle);
-		resetPage();
-	}
-	mySeconds--;
-	
-	
-}
-function startCounter(){
-	var myInput=document.getElementById("minutes").value;
-	if (isNaN(myInput)){
-		alert("Type a valid number please");
-		return;
-	}
-	mySeconds=myInput*60;
-	
-	intervalHandle=setInterval(tick, 1000);
-	
-	document.getElementById("inputArea").style.display="none";
-	
-	
+function start(){
+  countDownSeconds--;
+  document.getElementById("timerr").innerHTML = countDownSeconds;
+  if (countDownSeconds == -1){
+    stop();
+    document.getElementById("timerr").innerHTML = "0";  
+  }
 }
 
-
-window.onload=function(){
-	var myInput=document.createElement("input");
-	myInput.setAttribute("type","text");
-	myInput.setAttribute("id","minutes");
-	
-	var myButton=document.createElement("input");
-	myButton.setAttribute("type","button");
-	myButton.setAttribute("value","Start Timer");
-	
-	myButton.onclick=function(){
-		startCounter();	
-		
-	}
-	document.getElementById("inputArea").appendChild(myInput);
-	document.getElementById("inputArea").appendChild(myButton);
-	
-	
+function stop(){
+  clearInterval(myVar);
 }
