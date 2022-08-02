521587b76605b5581116e1432c51ac250205bd65
rockaguirre <107651547+rockaguirre@users.noreply.github.com>
2022-08-01 22:31:17 -0700

    ndinc

--- blocking.js
+++ blocking.js
@@ -1,16 +1,10 @@
-/* https://www.youtube.com/watch?v=dIrXIJ781DQ */
-
-//let cb = document.querySelector('#blocked_button');
-if (window.location.hostname =="www.youtube.come")
-{
-    alert("this is youtube");
-}
-//Check the state of the checked attribute
-/*if (cb.checked)
-{
-    if (window.location.hostname == "www.youtube.com"){
+blocker = function blocking (link) {
+    var current = window.location.href;
+    if (current.includes(link)) {
+        document.body.innerHTML = "";
         document.body.innerHTML = "Blocked LOL";
     }
-}*/
+}
 
+blocker("youtube.com");
 
