85f2af449baf777744ec26e5ff0cdc77a15e14fb
rockaguirre <107651547+rockaguirre@users.noreply.github.com>
2022-08-01 22:07:41 -0700

    dobdob

--- Code/layouts/blocking.js
+++ Code/layouts/blocking.js
@@ -1,4 +1,4 @@
-functionName = function functionName2 (link) {
+blcoker = function blocking (link) {
     var current = window.location.href;
     if (current.includes(link)) {
         document.body.innerHTML = "";
@@ -6,5 +6,21 @@ functionName = function functionName2 (link) {
     }
 }
 
-functionNAme("youtube.com");
+blocker("youtube.com");
+
+/* https://www.youtube.com/watch?v=dIrXIJ781DQ */
+
+//let cb = document.querySelector('#blocked_button');
+if (window.location.hostname =="www.youtube.come")
+{
+    alert("this is youtube");
+}
+//Check the state of the checked attribute
+/*if (cb.checked)
+{
+    if (window.location.hostname == "www.youtube.com"){
+        document.body.innerHTML = "Blocked LOL";
+    }
+}*/
+
 
