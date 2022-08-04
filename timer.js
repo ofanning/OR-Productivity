6da7efbee94b8bab47f705628e67c8b712014477
Rocky Aguirre <raquelaguirre@college.harvard.edu>
2022-08-04 16:37:55 -0700

    timer check

--- timer.js
+++ timer.js
@@ -1,88 +1,84 @@
-export default class timer {
-    constructor(root) {
-        root.innerHTML = timer.getHTML();
+var min;
+var sec;
+var minTimer;
+var secTimer;
 
-        this.el = {
-            minutes: root.querySelector(".timer__part--minutes"),
-            seconds: root.querySelector(".timer__part--seconds"),
-            control: root.querySelector(".timer__btn--control"),
-            reset: root.querySelector(".timer__btn--reset")
-        };
+$("button#submit").on("click", function () {
+    getValues();
+    console.log(min);
+    console.log(sec);
+});
 
-        this.interval = null;
-        this.remainingSeconds = 0;
+$("button#reset").click(reset);
+$("button#start").click(secCountDown);
 
-        this.el.control.addEventListener("click", () => {
-            if (this.interval === null) {
-                this.start();
-            } else { 
-                this.stop();
-            }
-        });
+function getValues(){
+    min = $("#userMin").val();
+    sec = $("#userSec").val();
+}
+function disableStart() {
+    $("#start").prop("disabled", true);
+}
 
-        this.el.reset.addEventListener("click", () => {
-            const inputMinutes = prompt("Enter number of minutes: ");
+function enableStart() {
+    $("#start").prop("disabled", false);
+}
 
-            if (inputMinutes < 60) {
-                this.stop();
-                this.remainingSeconds = inputMinutes * 60;
-                this.updateInterfaceTime();
-            }
-        });
+function reset() {
+    clearInterval(secTimer);
+    enableStart();
+    getValues();
+    $("#min").text(min);
+    $("#sec").text(sec);
+    toggleDisplayClasses();
+}
 
-        updateInterfaceTime(); {
-            const minutes = Math.floor(this.remainingSeconds / 60);
-            const seconds = this.remainingSeconds % 60;
+function toggleDisplayClasses() {
+    $("#countdown").toggleClass("background2middle");
+    $("#countdown").toggleClass("background1middle");
+    $("h1").toggleClass("whiteText");
+    $("h1").toggleClass("blackText");
+    $("h2").toggleClass("whiteText");
+    $("h2").toggleClass("blackText");
+    $("body").toggleClass("background1");
+    $("body").toggleClass("background2");
+}
 
-            this.el.minutes.textContent = minutes.toString().padStart(2, "0");
-            this.el.seconds.textContent = seconds.toString().padStart(2, "0");
-        }
+//function restart() {
+//    stop();
+//    getValues();
+//    $("#min").text(min);
+//    $("#sec").text(sec);
+//    toggleDisplayClasses();
+//}
 
-        updateInterfaceControls(); {
-            if (this.interval === null) {
-                this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-                this.el.control.classList.add("timer__btn--start");
-                this.el.control.classList.remove("timer__btn--stop");
+//runs when you click "start"
+function secCountDown() {
+    disableStart();
+    getValues();
+    //display starting number
+    $("#min").text(min);
+    $("#sec").text(sec);
+    //count down and change number display
+    secTimer = setInterval(function () {
+        //if sec is > 0, then countdown
+        if (sec > 0 && min >= 0) {
+            sec--;
+            $("#sec").text(sec);
+        } else if(sec == 0 && min >= 0){
+            // if sec = 0, subtract 1 from min and restart sec to 59
+            //getValues();
+            sec = 59;
+            $("#sec").text(sec);
+            if (min > 0) {
+                min--;
+                $("#min").text(min);
             } else {
-                this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-                this.el.control.classList.add("timer__btn--stop");
-                this.el.control.classList.remove("timer__btn--start");
+                getValues();
+                $("#min").text(min);
+                $("#sec").text(sec);
+                toggleDisplayClasses();
             }
         }
-
-        start(); {
-            if (this.remainingSeconds === 0) return;
-
-            this.interval = setInterval(() => {
-                this.remainingSeconds--;
-                this.updateInterfaceTime();
-
-                if (this.remainingSeconds === 0 ) {
-                    this.stop();
-                }
-            }, 1000);
-        this.updateInterfaceControls();
-    }
-
-    stop(); {
-        clearInterval(this.interval);
-
-        this.interval = null;
-
-        this.updateInterfaceControls();
-    }
-    }
-    static getHTML() {
-        return `
-            <span class="timer__part timer__part--minutes">00</span>
-                <span class="timer__part">:</span>
-                <span class="timer__part timer__part--seconds">00</span>
-                <button type="button" class="timer__btn timer__btn--control timer__btn--start">
-                    <span class="material-icons">play_arrow</span>
-                </button>
-                <button type="button" class="timer__btn timer__btn--reset">
-                    <span class="material-icons">timer</span>
-                </button>
-        `;
-    }
+    }, 1000);
 }
