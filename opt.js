7101515c8b3ac99c5e11dad51f98e864bccd47cb
ofanning <ofanning@college.harvard.edu>
2022-08-04 23:11:09 -0400

    beginning progress in opt.js

--- opt.js
+++ opt.js
@@ -1,11 +1,62 @@
+ 
 //build an array or object of the stuff from the user we need to save
 //probably need the array for the user urls and then something else for the toggle button?
 
+function buildSaveArray() {
+    //blank array that will be populated with user urls
+    let saveArray = [];
 
+    //get an HTMLcollection of the user urls then put them into an actual array
+    //so that they can be operated on with js
+    let collection = document.getElementsByClassName('userUrl');
+    for (let i = 0; i < elements.length; i++){
+        saveArray[i] = collection[i];
+    }
+
+    //call save_options with our newly minted array
+    saveUrls(saveArray)
+}
+
+//gets the value of the toggle html button, which defaults to
+//"on" when it is checked, and return a boolean value
+function getToggleValue() {
+    if (document.getElementById('block_toggle').value == "on") {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //save the array/object using chrome.storage.sync.set or chrome.storage.local.set
+function saveURLS(saveArray) {
+    //save the url array with the chrome storage api
+    chrome.storage.sync.set({
+        urlArray: saveArray
+    }, function() {
+        // this function will show the user that their options are saved
+        let status = document.getElementById('status');
+        status.textContent = 'options saved.';
+        //timer to change the status back to unsaved??? maybe remove
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+    //might need to add part saving the toggle value, but I think probably not
+} 
+
+
+
+
 
 
 
 
 //new function to restore all the user inputs each time the opt.html is opened
+function restoreUrls() {
+    chrome.storage.sync.get({
+        urlArray: []
+
+    }, function(items) {
+        buildOptDisplay(items.urlArray);
+    });
+}
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('*****************content script*******************')
+
 //This page will actually take the packaged user input from opt.js and change the body where there is a url match
 
 
